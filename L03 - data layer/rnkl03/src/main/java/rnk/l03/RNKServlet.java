b40047bd802207a88c10dd86f53920851f4dc37d
drJabber <dJabber@gmail.com>
2018-10-06 00:42:40 +0300

    add R&K tables population

--- L03 - data layer/rnkl03/src/main/java/rnk/l03/RNKServlet.java
+++ L03 - data layer/rnkl03/src/main/java/rnk/l03/RNKServlet.java
@@ -20,31 +20,207 @@ import rnk.l03.utils.Loader;
 @WebServlet("/rnk_jdbc")
 public class RNKServlet extends HttpServlet {
 
+
     @Resource(name="jdbc/rnk_db")
     private DataSource ds;
+
+    private void saveAuthorities(Connection conn, HttpServletResponse rsp, Loader loader) throws ServletException,IOException {
+        try(
+                PreparedStatement q=conn.prepareStatement(
+                        "insert into authorities(id, authority) values(?,?)");
+        )
+        {
+            ArrayList<String> authorities=loader.getAuthorities();
+            int idx=1;
+            for (String authority: authorities
+            ) {
+                q.setString(2,authority);
+                q.setInt(1,idx);
+                q.executeUpdate();
+                idx++;
+                rsp.getWriter().println("Authority '"+authority+"' created");
+            }
+
+        }catch(SQLException ex)
+        {
+            throw new ServletException(ex);
+        }
+
+    }
+
+    private void saveRoles(Connection conn, HttpServletResponse rsp, Loader loader) throws ServletException,IOException {
+        try(
+                PreparedStatement q=conn.prepareStatement(
+                        "insert into roles(role) values(?)");
+        )
+        {
+            ArrayList<String> roles=loader.getRoles();
+            int idx=1;
+            for (String role: roles
+            ) {
+                q.setString(1,role);
+                q.executeUpdate();
+                idx++;
+                rsp.getWriter().println("Role '"+role+"' created");
+            }
+
+        }catch(SQLException ex)
+        {
+            throw new ServletException(ex);
+        }
+
+    }
+
+    private void saveRoleAuth(Connection conn, HttpServletResponse rsp, Loader loader) throws ServletException,IOException {
+        try(
+                PreparedStatement q=conn.prepareStatement(
+                        "insert into role_auth(role_id,auth_id) values(?,?)");
+        )
+        {
+            ArrayList<String []> role_auths=loader.getRoleAuth();
+            int idx=1;
+            for (String[] role_auth: role_auths
+            ) {
+                q.setInt(1, Integer.parseInt(role_auth[0]));
+                q.setInt(2, Integer.parseInt(role_auth[1]));
+                q.executeUpdate();
+                idx++;
+                rsp.getWriter().println("Role_auth '"+role_auth[0]+" "+role_auth[1]+"' created");
+            }
+
+        }catch(SQLException ex)
+        {
+            throw new ServletException(ex);
+        }
+
+    }
+
+    private void savePositions(Connection conn, HttpServletResponse rsp, Loader loader) throws ServletException,IOException {
+        try(
+                PreparedStatement q=conn.prepareStatement(
+                        "insert into positions(position, default_dept_id, head_id, default_role_id, default_salary) values(?,?,?,?,?)");
+        )
+        {
+            ArrayList<String []> positions=loader.getPositions();
+            int idx=1;
+            for (String[] position: positions
+            ) {
+                q.setString(1, position[0]);
+                q.setInt(2, Integer.parseInt(position[1]));
+
+                String p=position[2];
+                if (p.isEmpty()){
+                    q.setNull(3, Types.INTEGER);
+                }else{
+                    q.setInt(3, Integer.parseInt(p));
+                }
+                q.setInt(4, Integer.parseInt(position[3]));
+                q.setInt(5, Integer.parseInt(position[4]));
+                q.executeUpdate();
+                idx++;
+                rsp.getWriter().println("position '"+position[0]+"' created");
+            }
+
+        }catch(SQLException ex)
+        {
+            throw new ServletException(ex);
+        }
+
+    }
+
+    private void saveDepartaments(Connection conn, HttpServletResponse rsp, Loader loader) throws ServletException,IOException {
+        try(
+                PreparedStatement q=conn.prepareStatement(
+                        "insert into departaments(departament,head_dept_id,head_of_dept_id,town) values(?,?,?,?)");
+        )
+        {
+            ArrayList<String []> departaments=loader.getDepartaments();
+            int idx=1;
+            for (String[] departament: departaments
+            ) {
+                q.setString(1, departament[0]);
+
+                String p=departament[1];
+                if (p.isEmpty()){
+                    q.setNull(2, Types.INTEGER);
+                }else{
+                    q.setInt(2, Integer.parseInt(p));
+                }
+
+                p=departament[2];
+                if (p.isEmpty()){
+                    q.setNull(3, Types.INTEGER);
+                }else{
+                    q.setInt(3, Integer.parseInt(p));
+                }
+
+                q.setString(4, departament[3]);
+                q.executeUpdate();
+                idx++;
+                rsp.getWriter().println("departament '"+departament[0]+"' created");
+            }
+
+        }catch(SQLException ex)
+        {
+            throw new ServletException(ex);
+        }
+
+    }
+
+    private void saveStaff(Connection conn, HttpServletResponse rsp, Loader loader) throws ServletException,IOException {
+        try(
+                PreparedStatement q=conn.prepareStatement(
+                        "insert into staff(fio,position_id,departament_id,salary,role_id,login,passwd_hash,passwd_salt) values(?,?,?,?,?,?,?,?)");
+        )
+        {
+            ArrayList<String []> staff=loader.getStaff();
+            int idx=1;
+            for (String[] person: staff
+            ) {
+                q.setString(1, person[0]);
+                q.setInt(2, Integer.parseInt(person[1]));
+                q.setInt(3, Integer.parseInt(person[2]));
+                q.setInt(4, Integer.parseInt(person[3]));
+                q.setInt(5, Integer.parseInt(person[4]));
+                q.setString(6, person[5]);
+                q.setString(7, person[6]);
+                q.setString(8, person[7]);
+                q.executeUpdate();
+                idx++;
+                rsp.getWriter().println("person '"+person[0]+"' created");
+            }
+
+        }catch(SQLException ex)
+        {
+            throw new ServletException(ex);
+        }
+
+    }
 
     @Override
     protected void doGet(HttpServletRequest rq, HttpServletResponse rsp) throws ServletException, IOException {
         try(
             Connection cn=ds.getConnection();
             PreparedStatement q=cn.prepareStatement(
-                    "select s.fio, p.position from staff s " +
-                            "join positions p on p.id=s.position_id");
+                    "select s.fio, p.position, d.departament from staff s " +
+                            "join positions p on p.id=s.position_id "+
+                            "join departaments d on d.id=s.departament_id ");
             ResultSet rs=q.executeQuery())
-        {    PrintWriter pw=rsp.getWriter();
+        {
             StringBuilder sb=new StringBuilder();
             while (rs.next())
             {
                 sb.append(
                         Stream.of(
-                                rs.getString("fio")+" "+
-                                rs.getString("position")
+                                rs.getString("fio:")+" "+
+                                rs.getString("position")+" at "+
+                                rs.getString("departament")
 
                         ).collect(Collectors.joining("|"))
                 );
             }
 
-            pw.println(sb.toString());
+            rsp.getWriter().println(sb.toString());
 
 
         }catch(SQLException ex)
@@ -55,25 +231,17 @@ public class RNKServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest rq, HttpServletResponse rsp) throws ServletException, IOException {
-        try(
-                Connection cn=ds.getConnection();
-                PreparedStatement q=cn.prepareStatement(
-                        "insert into authorities(id, authority) values(?,?)");
-                )
-        {
+
+        try(Connection cn=ds.getConnection()){
             Loader loader=new Loader();
             loader.loadAll();
-            ArrayList<String> authorities=loader.getAuthorities();
-            int idx=1;
-            for (String authority: authorities
-                 ) {
-                q.setString(2,authority);
-                q.setInt(1,idx);
-                q.executeUpdate();
-                idx++;
-                rsp.getWriter().println("Authority '"+authority+"' created");
-            }
 
+            saveAuthorities(cn,rsp, loader);
+            saveRoles(cn,rsp,loader);
+            saveRoleAuth(cn,rsp,loader);
+            saveDepartaments(cn,rsp,loader);
+            savePositions(cn,rsp,loader);
+            saveStaff(cn,rsp,loader);
         }catch(SQLException ex)
         {
             throw new ServletException(ex);
--- L03 - data layer/rnkl03/src/main/java/rnk/l03/utils/Loader.java
+++ L03 - data layer/rnkl03/src/main/java/rnk/l03/utils/Loader.java
@@ -1,40 +1,159 @@
 package rnk.l03.utils;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import lombok.Data;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-
+@Data
 public class Loader {
 
     private ArrayList<String> authorities;
+    private ArrayList<String> roles;
+    private ArrayList<String[]> departaments;
+    private ArrayList<String[]> positions;
+    private ArrayList<String[]> staff;
+    private ArrayList<String[]> role_auth;
 
     private void loadAuthorities() throws IOException {
         authorities.clear();
-        InputStream stream=Loader.class.getResourceAsStream("/authorities.csv");
-        try(
-            CSVReader reader=new CSVReader(new InputStreamReader(stream),';')){
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        try(InputStream stream=Loader.class.getResourceAsStream("/authorities.csv");
+            CSVReader reader=new CSVReaderBuilder(new InputStreamReader(stream))
+                                          .withSkipLines(1)
+                                          .withCSVParser(parser).build()
+        ){
             String[] csvLine;
-
-            reader.readNext();//skip header
             while ((csvLine=reader.readNext())!=null){
                 authorities.add(csvLine[1]);
             }
         }
     }
 
+    private void loadRoles() throws IOException {
+        roles.clear();
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        try(InputStream stream=Loader.class.getResourceAsStream("/roles.csv");
+            CSVReader reader=new CSVReaderBuilder(new InputStreamReader(stream))
+                    .withSkipLines(1)
+                    .withCSVParser(parser).build()
+        ){
+            String[] csvLine;
+            while ((csvLine=reader.readNext())!=null){
+                roles.add(csvLine[0]);
+            }
+        }
+    }
+
+    private void loadRoleAuth() throws IOException {
+        role_auth.clear();
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        try(InputStream stream=Loader.class.getResourceAsStream("/role_auth.csv");
+            CSVReader reader=new CSVReaderBuilder(new InputStreamReader(stream))
+                    .withSkipLines(1)
+                    .withCSVParser(parser).build()
+        ){
+            String[] csvLine;
+            while ((csvLine=reader.readNext())!=null){
+                role_auth.add(csvLine);
+            }
+        }
+    }
+
+
+    private void loadPositions() throws IOException {
+        positions.clear();
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        try(InputStream stream=Loader.class.getResourceAsStream("/positions.csv");
+            CSVReader reader=new CSVReaderBuilder(new InputStreamReader(stream))
+                    .withSkipLines(1)
+                    .withCSVParser(parser).build()
+        ){
+            String[] csvLine;
+            while ((csvLine=reader.readNext())!=null){
+                positions.add(csvLine);
+            }
+        }
+    }
+
+    private void loadDepartaments() throws IOException {
+        departaments.clear();
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        try(InputStream stream=Loader.class.getResourceAsStream("/departaments.csv");
+            CSVReader reader=new CSVReaderBuilder(new InputStreamReader(stream))
+                    .withSkipLines(1)
+                    .withCSVParser(parser).build()
+        ){
+            String[] csvLine;
+            while ((csvLine=reader.readNext())!=null){
+                departaments.add(csvLine);
+            }
+        }
+    }
+
+    private void loadStaff() throws IOException {
+        staff.clear();
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        try(InputStream stream=Loader.class.getResourceAsStream("/staff.csv");
+            CSVReader reader=new CSVReaderBuilder(new InputStreamReader(stream))
+                    .withSkipLines(1)
+                    .withCSVParser(parser).build()
+        ){
+            String[] csvLine;
+            while ((csvLine=reader.readNext())!=null){
+                staff.add(csvLine);
+            }
+        }
+    }
+
+
     public Loader(){
         authorities=new ArrayList<String>();
+        roles=new ArrayList<String>();
+        role_auth=new ArrayList<String[]>();
+        positions=new ArrayList<String[]>();
+        departaments=new ArrayList<String[]>();
+        staff=new ArrayList<String[]>();
     }
 
     public void loadAll() throws IOException{
         loadAuthorities();
+        loadRoles();
+        loadRoleAuth();
+        loadDepartaments();
+        loadPositions();
+        loadStaff();
+
     }
 
     public ArrayList<String> getAuthorities(){
         return authorities;
     }
+
+    public ArrayList<String> getRoles(){
+        return roles;
+    }
+
+    public ArrayList<String[]> getRoleAuth(){
+        return role_auth;
+    }
+
+    public ArrayList<String[]> getPositions(){
+        return positions;
+    }
+
+    public ArrayList<String[]> getDepartaments(){
+        return departaments;
+    }
+
+    public ArrayList<String[]> getStaff(){
+        return staff;
+    }
+
 }
