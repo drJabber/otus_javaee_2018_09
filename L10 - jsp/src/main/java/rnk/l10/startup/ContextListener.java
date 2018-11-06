5e18dfa966b0ccde7b871eea4705ed65283c1f06
drJabber@gmail.com <dJabber@gmail.com>
2018-11-06 14:14:50 +0300

    intermediate

--- L10 - jsp/src/main/java/rnk/l10/startup/ContextListener.java
+++ L10 - jsp/src/main/java/rnk/l10/startup/ContextListener.java
@@ -1,11 +1,25 @@
 package rnk.l10.startup;
 
+import org.apache.log4j.Logger;
+import rnk.l10.entities.xml.StaffEntitiesList;
+
+import javax.persistence.*;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebListener;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @WebListener()
 public class ContextListener implements ServletContextListener{
+    public static final String PERSISTENCE_UNIT_NAME="rnk-jpa";
+    private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME); //tomcat, see
+    private static final Logger logger = Logger.getLogger(ContextListener.class.getName());
 
     // -------------------------------------------------------
     // ServletContextListener implementation
@@ -22,10 +36,52 @@ public class ContextListener implements ServletContextListener{
          (the Web application) is undeployed or 
          Application Server shuts down.
       */
-      Object o=sce.getServletContext().getAttribute("startup");
-      if ((o!=null) && (o instanceof Startup)){
-          Startup startup=(Startup)o;
+      try{
+          ServletContext context=sce.getServletContext();
+          Object o=context.getAttribute("startup");
+          if ((o!=null) && (o instanceof Startup)){
+              Startup startup=(Startup)o;
+              if (startup.getInitialized()==1){
+                  make_xml(get_filename(context));
+              }
+          }
+      }catch(Exception ex){
+          logger.error("Error while savind DB state to XML:", ex);
       }
+
+    }
+
+    private String get_filename(ServletContext context){
+        return context.getAttribute(ServletContext.TEMPDIR)+"/xml/employees.xml";
+    }
+
+    private void make_xml(String filename) throws ServletException {
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction et=em.getTransaction();
+        try{
+            et.begin();
+            Query q = em.createQuery(
+                    "select staff "+
+                            "from StaffEntity staff "+
+                            "order by staff.id desc");
+            StaffEntitiesList sl=new StaffEntitiesList();
+            sl.setStaff_list(q.getResultList());
+
+            JAXBContext jc=JAXBContext.newInstance(sl.getClass());
+            Marshaller m=jc.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,Boolean.TRUE);
+            Path path= Paths.get(filename);
+            Files.createDirectories(path.getParent());
+            m.marshal(sl, path.toFile());
+
+            et.commit();
+        }catch(Exception ex){
+            et.rollback();
+            throw new ServletException(ex);
+        }
+        finally{
+            em.close();
+        }
     }
 
 }
