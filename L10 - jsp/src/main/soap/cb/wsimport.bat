f6e002e361f5c057bee2ea0de6d2c2f180ea8928
Sutyagin <Sutyagin@NFTG.local>
2018-11-29 13:01:01 +0300

    fix

---
+++ L10 - jsp/src/main/soap/cb/wsimport.bat
@@ -0,0 +1,2 @@
+
+"D:\Program Files\Java\jdk1.8.0_144\bin\wsimport.exe" -d java  -target 2.0 -p rnk.l10.soap.cbko -extension -keep -verbose -b http://www.w3.org/2001/XMLSchema.xsd -b ./cbr_custom.xjb -J"-Djavax.xml.accessExternalDTD=all" http://www.cbr.ru/CreditInfoWebServ/CreditOrgInfo.asmx?WSDL 
