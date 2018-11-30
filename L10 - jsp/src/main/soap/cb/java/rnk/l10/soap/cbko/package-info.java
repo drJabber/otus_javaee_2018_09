c9ff884bbbac1267bc9ff2f42bb127e2d8569b40
drJabber@gmail.com <dJabber@gmail.com>
2018-11-30 17:11:17 +0300

    fix

--- L10 - jsp/src/main/soap/cb/java/rnk/l10/soap/cbko/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Справочник по кредитным организациям, ver - 07.03.2017
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://web.cbr.ru/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package rnk.l10.soap.cbko;
