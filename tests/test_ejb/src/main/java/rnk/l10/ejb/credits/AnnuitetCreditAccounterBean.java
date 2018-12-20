125ea80c1c817fe0219c1eafb659fcb75a2bde5f
Sutyagin <Sutyagin@NFTG.local>
2018-12-20 09:01:40 +0300

    test case for ejb

---
+++ tests/test_ejb/src/main/java/rnk/l10/ejb/credits/AnnuitetCreditAccounterBean.java
@@ -0,0 +1,25 @@
+package rnk.l10.ejb.credits;
+
+import rnk.l10.rest.model.AccountingParams;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+public class AnnuitetCreditAccounterBean implements RnkCreditAccounterV2 {
+    @Override
+    public List<Double> computePayment(AccountingParams params) {
+        List<Double> result=new ArrayList<>();
+        int T=params.getNumberOfPeriods();
+        double Kr=params.getAmountOfCredit();
+        double R=params.getRate();
+        double P=Kr*R/(1-1/Math.pow((1+R),T));
+        for (int i=0; i<T;i++){
+            result.add(P);
+        }
+        return result;
+
+    }
+}
