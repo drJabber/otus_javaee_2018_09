a0cff9edf24cbe46bae59dc97bb0957a347d7712
drJabber@gmail.com <dJabber@gmail.com>
2018-12-21 08:55:48 +0300

    exb test fix by Vitalii

--- tests/test_ejb/src/main/java/rnk/t01/rest/TestServiceImpl.java
+++ tests/test_ejb/src/main/java/rnk/t01/rest/TestServiceImpl.java
@@ -3,6 +3,7 @@ package rnk.t01.rest;
 import org.apache.log4j.Logger;
 import rnk.t02.soap.SnilsChecker;
 
+import javax.ejb.EJB;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.GET;
@@ -19,16 +20,8 @@ public class TestServiceImpl {
 
     private static InitialContext defaultCtx;
 
-//    Portable JNDI names for EJB SnilsCheckerWebService:
-// [java:global/rnkt02/SnilsCheckerWebService,
-//  java:global/rnkt02/SnilsCheckerWebService!rnk.t02.soap.SnilsChecker]|#]
-
-//    Glassfish-specific (Non-portable) JNDI names for EJB SnilsCheckerWebService:
-// [rnk.t02.soap.SnilsChecker,
-// rnk.t02.soap.SnilsChecker#rnk.t02.soap.SnilsChecker]|#]
-
-//   @EJB(lookup = "ejb/rnk.l10.snilschecker")
-//   SnilsChecker bean;
+    @EJB(lookup = "java:global/rnkt02/SnisChecker!rnk.t02.soap.SnilsChecker")
+    SnilsChecker snilsChecker;
 
     static {
         try {
@@ -45,15 +38,14 @@ public class TestServiceImpl {
     @GET
     @Path("/start")
     public Map<String, String> startTest() throws NamingException{
-        SnilsChecker snilsChecker=
-                (SnilsChecker)defaultCtx.lookup(
-                         "java:global/rnkt02/SnilsCheckerWebService!rnk.t02.soap.SnilsChecker"
-                );
+//        SnilsChecker snilsChecker=
+//                (SnilsChecker)defaultCtx.lookup(
+//                         "java:global/rnkt02/SnisChecker!rnk.t02.soap.SnilsChecker"
+//                );
         Map<String, String> result=new HashMap<>();
-//        Boolean b=bean.check("001-280-213-70");
-        Boolean b=true;
+        Boolean b= snilsChecker.check("001-280-213-70");
         result.put("snilscherker.check('001-280-213-70')",b.toString());
-//        b=bean.check("001-280-213-74");
+        b = snilsChecker.check("001-280-213-74");
         result.put("snilscherker.check('001-280-213-74')",b.toString());
         return result;
     }
